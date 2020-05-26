6b78413b68cacd7008a79afb22de93bdc24168f6
Erik Bernskiold <erik@bernskioldmedia.com>
2020-05-26 20:55:56 +0200

    Data classes for speakers and sessions

---
+++ src/data/session.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Session Data
+ *
+ * @author  Bernskiold Media <info@bernskioldmedia.com>
+ * @package BernskioldMedia\WP\Event
+ * @since   1.0.0
+ */
+
+namespace BernskioldMedia\WP\Event\Data;
+
+use BernskioldMedia\WP\Event\Abstracts\Data;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Session
+ *
+ * @package BernskioldMedia\WP\Event
+ */
+class Session extends Data {
+
+	/**
+	 * Get Session Title
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return get_the_title( $this->get_id() );
+	}
+
+	/**
+	 * Get session label.
+	 *
+	 * @return string|null
+	 */
+	public function get_label(): ?string {
+		return $this->get_prop( 'session_label' );
+	}
+
+	/**
+	 * Get the session short description
+	 *
+	 * @return string|null
+	 */
+	public function get_short_description(): ?string {
+		return $this->get_prop( 'session_short_description' );
+	}
+
+	/**
+	 * Get the type of the session with both value and label.
+	 *
+	 * @return array|null
+	 */
+	public function get_session_type(): ?array {
+		return $this->get_prop( 'session_type' );
+	}
+
+	/**
+	 * Get the session type key (value).
+	 *
+	 * @return string|null
+	 */
+	public function get_session_type_key(): ?string {
+		$session_type = $this->get_session_type();
+
+		return $session_type ? $session_type['value'] : null;
+	}
+
+	/**
+	 * Get the session type label. Localized.
+	 *
+	 * @return string|null
+	 */
+	public function get_session_type_label(): ?string {
+		$session_type = $this->get_session_type();
+
+		return $session_type ? $session_type['label'] : null;
+	}
+
+	/**
+	 * Get the date the session is at.
+	 *
+	 * @param  string  $format  date() compatible string.
+	 *
+	 * @return string|null
+	 */
+	public function get_date( string $format = 'Y-m-d' ): ?string {
+		$date = $this->get_prop( 'session_date' );
+
+		if ( ! $date ) {
+			return null;
+		}
+
+		return wp_date( $format, strtotime( $date ) );
+	}
+
+	/**
+	 * Get the time when the session starts.
+	 *
+	 * @param  string  $format  date() compatible format.
+	 *
+	 * @return string|null
+	 */
+	public function get_start_time( string $format = 'H:i' ): ?string {
+		$time = $this->get_prop( 'session_start_time' );
+
+		if ( ! $time ) {
+			return null;
+		}
+
+		return wp_date( $format, strtotime( $time ) );
+	}
+
+	/**
+	 * Get the time when the session ends.
+	 *
+	 * @param  string  $format  date() compatible format.
+	 *
+	 * @return string|null
+	 */
+	public function get_end_time( string $format = 'H:i' ): ?string {
+		$time = $this->get_prop( 'session_end_time' );
+
+		if ( ! $time ) {
+			return null;
+		}
+
+		return wp_date( $format, strtotime( $time ) );
+	}
+
+	/**
+	 * Get an array of speaker IDs associated with this session.
+	 *
+	 * @return array|null
+	 */
+	public function get_speaker_ids(): ?array {
+		return $this->get_prop( 'session_speakers_ids' );
+	}
+
+	/**
+	 * Get the speakers (with objects) associated with this session.
+	 *
+	 * @return array|null
+	 */
+	public function get_speakers(): ?array {
+		$ids = $this->get_speaker_ids();
+
+		if ( ! $ids ) {
+			return null;
+		}
+
+		$speakers = [];
+
+		foreach ( $ids as $id ) {
+			$speakers[] = new Speaker( $id );
+		}
+
+		return $speakers;
+	}
+
+}
---
+++ src/data/speaker.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Speaker Data
+ *
+ * @author  Bernskiold Media <info@bernskioldmedia.com>
+ * @package BernskioldMedia\WP\Event
+ * @since   1.0.0
+ */
+
+namespace BernskioldMedia\WP\Event\Data;
+
+use BernskioldMedia\WP\Event\Abstracts\Data;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Speaker
+ *
+ * @package BernskioldMedia\WP\Event
+ */
+class Speaker extends Data {
+
+	/**
+	 * Get Name
+	 *
+	 * @return string|null
+	 */
+	public function get_name(): ?string {
+		return $this->get_first_name() . ' ' . $this->get_last_name();
+	}
+
+	/**
+	 * Get First Name
+	 *
+	 * @return string|null
+	 */
+	public function get_first_name(): ?string {
+		return $this->get_prop( 'speaker_first_name' );
+	}
+
+	/**
+	 * Get Last Name
+	 *
+	 * @return string|null
+	 */
+	public function get_last_name(): ?string {
+		return $this->get_prop( 'speaker_last_name' );
+	}
+
+	/**
+	 * Get speaker company name.
+	 *
+	 * @return string|null
+	 */
+	public function get_company(): ?string {
+		return $this->get_prop( 'speaker_company' );
+	}
+
+	/**
+	 * Get speaker title / role at company.
+	 *
+	 * @return string|null
+	 */
+	public function get_title(): ?string {
+		return $this->get_prop( 'speaker_title' );
+	}
+
+	/**
+	 * Get the short biography for the speaker.
+	 *
+	 * @return string|null
+	 */
+	public function get_short_biography(): ?string {
+		return $this->get_prop( 'speaker_short_biography' );
+	}
+
+	/**
+	 * Get the speakers' website URL.
+	 *
+	 * @return string|null
+	 */
+	public function get_website_url(): ?string {
+		return $this->get_prop( 'speaker_website_url' );
+	}
+
+	/**
+	 * Get the speakers' twitter handle/username.
+	 *
+	 * @return string|null
+	 */
+	public function get_twitter_handle(): ?string {
+		return $this->get_prop( 'speaker_twitter_handle' );
+	}
+
+	/**
+	 * Get the URL to the speaker's Facebook page.
+	 *
+	 * @return string|null
+	 */
+	public function get_facebook_url(): ?string {
+		return $this->get_prop( 'speaker_facebook_url' );
+	}
+
+	/**
+	 * Get the speaker's Instagram username.
+	 *
+	 * @return string|null
+	 */
+	public function get_instagram_username(): ?string {
+		return $this->get_prop( 'speaker_instagram_username' );
+	}
+
+	/**
+	 * Get the speaker's YouTube channel URL.
+	 *
+	 * @return string|null
+	 */
+	public function get_youtube_url(): ?string {
+		return $this->get_prop( 'speaker_youtube_url' );
+	}
+
+	/**
+	 * Get the speaker's LinkedIn profile URL.
+	 *
+	 * @return string|null
+	 */
+	public function get_linkedin_url(): ?string {
+		return $this->get_prop( 'speaker_linkedin_url' );
+	}
+
+}
