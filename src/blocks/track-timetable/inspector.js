3775a8502d9293b2a44b4f101c1cc4a16cc1e41b
Erik Bernskiold <erik@bernskioldmedia.com>
2020-05-27 14:25:14 +0200

    Update inspector settings

--- src/blocks/track-timetable/inspector.js
+++ src/blocks/track-timetable/inspector.js
@@ -1,12 +1,4 @@
 /**
- * Internal Dependencies
- */
-import SectionAppearance from "../../components/section/inspector";
-import SectionHeaderInspector from "../../components/section-header/inspector";
-import SectionFooterInspector from "../../components/section-footer/inspector";
-import RecordSelector from "../../components/record-select/record-select";
-
-/**
  * WordPress Dependencies
  */
 const { __ }                = wp.i18n;
@@ -29,85 +21,25 @@ export default function Inspector( props ) {
 	return (
 		<InspectorControls>
 
-			<PanelBody title={__( "Events Style", "bm-block-upcoming-events" )}>
+			<PanelBody title={__( 'Events Style', 'bm-block-track-timetable' )}>
 				<SelectControl
-					label={__( "Upcoming Events Style", "bm-block-upcoming-events" )}
+					label={__( 'Timetable Style', 'bm-block-track-timetable' )}
 					value={attributes.style} // e.g: value = [ 'a', 'c' ]
 					onChange={( value ) => {
 						setAttributes( { style: value } );
 					}}
 					options={[
 						{
-							value: "small",
-							label: __( "Small List", "bm-block-upcoming-events" )
+							value: 'normal',
+							label: __( 'Normal', 'bm-block-track-timetable' )
 						},
 						{
-							value: "list",
-							label: __( "List", "bm-block-upcoming-events" )
-						},
-						{
-							value: "month",
-							label: __( "Month", "bm-block-upcoming-events" )
-						},
-						{
-							value: "week",
-							label: __( "Week", "bm-block-upcoming-events" )
+							value: 'mini',
+							label: __( 'Mini', 'bm-block-track-timetable' )
 						}
 					]}
 				/>
 			</PanelBody>
-
-			<PanelBody title={__( "Selection & Filters", "bm-block-upcoming-events" )}>
-
-				<RangeControl
-					label={__( "Amount of Events", "bm-block-upcoming-events" )}
-					min={3}
-					max={9}
-					value={attributes.amount}
-					beforeIcon="screenoptions"
-					allowReset={true}
-					onChange={( value ) => {
-						setAttributes( {
-							amount: value
-						} );
-					}}
-				/>
-
-				<PanelRow>
-					<p>{__( "By default the events block will show all the latest events in descending order. Below you can limit the selection.", "bm-block-upcoming-events" )}</p>
-				</PanelRow>
-
-				<BaseControl label={__( "Include only from specific categories", "bm-block-upcoming-events" )}>
-					<RecordSelector
-						value={attributes.inCategories}
-						placeholder={__( "Select categories...", "bm-block-upcoming-events" )}
-						type="taxonomy"
-						objectType="tribe_events_cat"
-						onChange={( selected ) => {
-							setAttributes( { inCategories: selected } );
-						}}
-						multiple={true}
-					/>
-				</BaseControl>
-
-				<BaseControl label={__( "Include only from specific tags.", "bm-block-upcoming-events" )}>
-					<RecordSelector
-						value={attributes.inTags}
-						placeholder={__( "Select tags...", "bm-block-upcoming-events" )}
-						type="taxonomy"
-						objectType="post_tag"
-						onChange={( selected ) => {
-							setAttributes( { inTags: selected } );
-						}}
-						multiple={true}
-					/>
-				</BaseControl>
-
-			</PanelBody>
-
-			<SectionAppearance {...props} />
-			<SectionHeaderInspector {...props} />
-			<SectionFooterInspector {...props} />
 
 		</InspectorControls>
 	);
